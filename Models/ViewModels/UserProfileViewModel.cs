5ebf717726e0175ba7daa30f4ea617962669bb79
Frzz23 <frenzdrez@gmail.com>
2025-06-24 13:09:43 +0545

    finished

---
+++ Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trash_Track.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        public int WardNumber { get; set; }
+
+        // Password change fields - optional but with validation when provided
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string? CurrentPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{6,}$",
+            ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
